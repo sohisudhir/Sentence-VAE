11fc691288e5f47c1de27930b7cf95705e151771
ximua00 <kishanarendra@gmail.com>
2019-05-18 18:53:45 +0200

    rare words, padding, EOS and SOS.

--- code/PennTreeData.py
+++ code/PennTreeData.py
@@ -2,22 +2,54 @@ from pprint import pprint
 import numpy as np
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from collections import Counter
 
 class PennTreeData(Dataset):
-    def __init__(self, data_path, seq_length):
-        self.seq_length = seq_length
+    def __init__(self, data_path, rare_threshold):
+        self.rare_threshold = rare_threshold
         self.data = self.remove_tags(open(data_path, "r").read())
-        self.vocab = list(set(self.data))
-        self.data_size, self.vocab_size  = len(self.data), len(self.vocab) 
-        self.word2idx= {w:i for i,w in enumerate(self.vocab)}
-        self.idx2word = {i:w for i,w in enumerate(self.vocab)}
-        
+        self.SOS = "|new"
+        self.EOS = "."
+        self.pad = "<_>"
+        self.rare = "RARE"
+        self.data.extend([self.SOS for i in range(self.rare_threshold + 1)])
+        self.data.extend([self.pad for i in range(self.rare_threshold + 1)])
+        self.data.extend([self.rare for i in range(self.rare_threshold + 1)])
 
+        self.vocab = list(set(self.data))        
+        self.define_mappings()
+
+        self.sentences = [sentence for sentence in self.read_sentence()]
+        self.max_sentence_len = self.largest_sentence()
+
+    def define_mappings(self):
+        self.word_counter = Counter(self.data)
+        self.word2idx = {}
+        self.idx2word = {}
+        idx = 0
+        for w in self.vocab:
+            if self.word_counter[w] > self.rare_threshold:
+                self.word2idx[w] = idx
+                self.idx2word[idx] = w
+                idx+=1
+        self.vocab_size = len(self.word2idx)
+        
     def __getitem__(self, idx):
-        offset = np.random.randint(0, len(self.data)-self.seq_length-2)
-        inputs =  [self.word2idx[w] for w in self.data[offset:offset+self.seq_length]]
-        targets = [self.word2idx[w] for w in self.data[offset+1:offset+self.seq_length+1]]
+        sentence = self.sentences[idx]
+        inputs =  [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence]
+        targets = [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence[1:]]
+        # add padding
+        inputs.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(inputs))])
+        targets.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(targets))])
         return inputs, targets
+
+    def read_sentence(self):
+        sentence = [self.SOS]
+        for word in self.data:
+            sentence.append(word)
+            if word == self.EOS:
+                yield sentence
+                sentence = [self.SOS]
 
     def remove_tags(self, data):
         """
@@ -31,6 +63,13 @@ class PennTreeData(Dataset):
                 out.append(e[-1])
         return out
 
+    def largest_sentence(self):
+        max_len = 0
+        for sentence in self.read_sentence():
+            if len(sentence) > max_len:
+                max_len = len(sentence)
+        return max_len
+
     def convert_to_string(self, char_idx):
         return ' '.join(self.idx2word[idx] for idx in char_idx)
 
@@ -38,17 +77,25 @@ class PennTreeData(Dataset):
         return [self.word2idx[char] for char in chars]
 
     def __len__(self):
-        return self.data_size
+        return len(self.sentences)
 
 
 if __name__ == "__main__":
-    path = "../data/23.auto.clean"
-    penntree = PennTreeData(data_path = path, seq_length = 5)
+    #path = "../data/02-21.10way.clean"
+    path = "../data/sample.clean"
+    penntree = PennTreeData(data_path = path, rare_threshold = 1)
+    
     data = DataLoader(penntree, batch_size=1, num_workers=1)
     for x, y in data:
         print(x)
-        print(y)
         break
+        
+        
+        
+    
+    
+        
+        
 
 
 
