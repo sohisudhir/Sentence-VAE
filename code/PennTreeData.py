b55248bd79d8310a406cc260d65b57b9b66238af
ximua00 <kishanarendra@gmail.com>
2019-05-23 19:54:52 +0200

    Fixed problems with data loader, added perplexity

--- code/PennTreeData.py
+++ code/PennTreeData.py
@@ -4,26 +4,26 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from collections import Counter
 
-class PennTreeData(Dataset):
-    def __init__(self, data_path, rare_threshold=1):
+class BaseTreeData(Dataset):
+    def __init__(self, train_data_path="../data/02-21.10way.clean", rare_threshold=1):
         self.rare_threshold = rare_threshold
-        self.data = self.remove_tags(open(data_path, "r").read())
+        self.base_data = self.remove_tags(open(train_data_path, "r").read())
         self.SOS = "|new"
         self.EOS = "."
         self.pad = "<_>"
         self.rare = "RARE"
-        self.data.extend([self.SOS for i in range(self.rare_threshold + 1)])
-        self.data.extend([self.pad for i in range(self.rare_threshold + 1)])
-        self.data.extend([self.rare for i in range(self.rare_threshold + 1)])
+        self.base_data.extend([self.SOS for i in range(self.rare_threshold + 1)])
+        self.base_data.extend([self.pad for i in range(self.rare_threshold + 1)])
+        self.base_data.extend([self.rare for i in range(self.rare_threshold + 1)])
 
-        self.vocab = list(set(self.data))        
+        self.vocab = list(set(self.base_data))        
         self.define_mappings()
 
-        self.sentences = [sentence for sentence in self.read_sentence()]
+        # self.sentences = [sentence for sentence in self.read_sentence(self.base_data)]
         self.max_sentence_len = self.largest_sentence()
 
     def define_mappings(self):
-        self.word_counter = Counter(self.data)
+        self.word_counter = Counter(self.base_data)
         self.word2idx = {}
         self.idx2word = {}
         idx = 0
@@ -35,18 +35,19 @@ class PennTreeData(Dataset):
         self.vocab_size = len(self.word2idx)
         
     def __getitem__(self, idx):
-        sentence = self.sentences[idx]
-        inputs =  [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence]
-        targets = [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence[1:]]
-        # add padding
-        inputs.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(inputs))])
-        targets.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(targets))])
-        batch = (inputs, targets)
-        return batch, len(sentence)
+        # sentence = self.sentences[idx]
+        # inputs =  [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence]
+        # targets = [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence[1:]]
+        # # add padding
+        # inputs.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(inputs))])
+        # targets.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(targets))])
+        # batch = (inputs, targets)
+        # return batch, len(sentence)
+        raise NotImplementedError
 
-    def read_sentence(self):
+    def read_sentence(self, data):
         sentence = [self.SOS]
-        for word in self.data:
+        for word in data:
             sentence.append(word)
             if word == self.EOS:
                 yield sentence
@@ -66,7 +67,7 @@ class PennTreeData(Dataset):
 
     def largest_sentence(self):
         max_len = 0
-        for sentence in self.read_sentence():
+        for sentence in self.read_sentence(self.base_data):
             if len(sentence) > max_len:
                 max_len = len(sentence)
         return max_len
@@ -78,14 +79,35 @@ class PennTreeData(Dataset):
         return [self.word2idx[char] for char in chars]
 
     def __len__(self):
+        # return len(self.sentences)
+        raise NotImplementedError
+
+
+class PennTreeData(BaseTreeData):
+    def __init__(self, data_path):
+        super().__init__()
+        self.data = self.remove_tags(open(data_path, "r").read())
+        self.sentences = [sentence for sentence in self.read_sentence(self.data)]
+
+    def __getitem__(self, idx):
+        sentence = self.sentences[idx]
+        inputs =  [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence]
+        targets = [self.word2idx[w] if self.word_counter[w] > self.rare_threshold else self.word2idx[self.rare] for w in sentence[1:]]
+        # add padding
+        inputs.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(inputs))])
+        targets.extend([self.word2idx[self.pad] for i in range(self.max_sentence_len - len(targets))])
+        batch = (inputs, targets)
+        return batch, len(sentence)
+        
+    def __len__(self):
         return len(self.sentences)
+        
 
 
 if __name__ == "__main__":
-    #path = "../data/02-21.10way.clean"
     path = "../data/sample.clean"
-    penntree = PennTreeData(data_path = path, rare_threshold = 1)
-    
+    penntree = PennTreeData(data_path = path)
+
     data = DataLoader(penntree, batch_size=1, num_workers=1)
     for x, y in data:
         print(x)
--- code/train.py
+++ code/train.py
@@ -4,33 +4,77 @@ from datetime import datetime
 import argparse
 
 import numpy as np
+from scipy.misc import logsumexp
 from matplotlib import pyplot as plt
 
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
 from PennTreeData import PennTreeData
 from RNNLM import RNNLM
 
 
-TEXT_FILE = "../data/02-21.10way.clean"
+TRAIN_TEXT_FILE = "../data/02-21.10way.clean"
+EVAL_TEXT_FILE = "../data/22.auto.clean"
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
 ################################################################################
+
+def dataset_metrics(dataloader, model):
+    n_sentences = 0
+    total_negative_ll = 0
+    accuracy = 0.0
+    with torch.no_grad():
+        for step, (batch, sentence_len) in enumerate(dataloader):
+        
+            batch_inputs, batch_targets = batch
+            batch_inputs = torch.stack(batch_inputs).to(device)
+            flat_targets = torch.stack(batch_targets).to(device).view(-1)
+            batch_targets = torch.stack(batch_targets).to(device)
+
+            output, _ = model(batch_inputs)
+            negative_ll, batch_size = calculate_perplexity(output , batch_targets, sentence_len)
+            output = output.view(batch_inputs.size()[0]*batch_inputs.size()[1], -1)
+            accuracy += calculate_accuracy(output, flat_targets)
+    
+            total_negative_ll += negative_ll
+            n_sentences += batch_size
+
+    total_perplexity = np.exp(total_negative_ll/n_sentences)
+
+    return total_perplexity, total_negative_ll, accuracy/(step + 1)
+
+
 def calculate_accuracy(predictions, targets):
     accurates = torch.argmax(predictions, dim=1) == targets
     accuracy = accurates.type(torch.FloatTensor).mean().item()
-
     return accuracy
 
-def calculate_perplexity(predictions, targets):
-    # TODO
-    pass
+
+def calculate_perplexity(predictions, targets, sentences_len):
+    predictions = F.softmax(predictions, dim=2)    
+    likelihoods = torch.gather(predictions, 2, targets.unsqueeze(dim=2).type(torch.LongTensor).to(device)).squeeze(dim=2)
+    batch_size = predictions.size()[1]
+    
+    total_likelihood = 0
+    for idx in range(batch_size): 
+        sentence = likelihoods[:, idx]
+        sentence_len = sentences_len[idx]
+        sentence = sentence[:sentence_len]
+        sentence_likelihood = logsumexp(sentence.detach().cpu().numpy())
+        total_likelihood += sentence_likelihood
+
+    return -np.log(total_likelihood), batch_size
+        
 
 def temperature_sample(h, temperature):
     h = h.squeeze()
-    distribution = torch.softmax(h/temperature, dim=0)    
+    distribution = F.softmax(h/temperature, dim=0)    
     return torch.multinomial(distribution,1).item()
 
 def generate_samples(model, dataset, device, temperature = 0.5, method = "greedy"):
@@ -70,19 +114,16 @@ def generate_sentence(model, sentence, device):
 
 def train(config):
 
-    # Initialize the device which to run the model on
-    device = torch.device(config.device)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-
     # Initialize the dataset and data loader (note the +1)
-    dataset = PennTreeData(TEXT_FILE, rare_threshold=1)
-    data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
-    
+    train_dataset = PennTreeData(TRAIN_TEXT_FILE)
+    train_data_loader = DataLoader(train_dataset, config.batch_size, num_workers=1)
+    eval_dataset = PennTreeData(EVAL_TEXT_FILE)
+    eval_data_loader = DataLoader(eval_dataset, config.batch_size, num_workers=1)
+
     # Initialize the model that we are going to use
-    model = RNNLM(seq_length=dataset.max_sentence_len,
+    model = RNNLM(seq_length=train_dataset.max_sentence_len,
                   embedding_dim=config.embedding_dim,
-                  vocabulary_size=dataset.vocab_size,
+                  vocabulary_size=train_dataset.vocab_size,
                   lstm_num_hidden=config.lstm_num_hidden,
                   lstm_num_layers=config.lstm_num_layers,
                   device=device)
@@ -92,27 +133,34 @@ def train(config):
     # f = open("../sample_results/samples_{}_{}.txt".format(config.sample_method, config.temperature),"w+")
 
     # Setup the loss and optimizer
-    criterion = nn.CrossEntropyLoss()
+    padding_index = train_dataset.word2idx[train_dataset.pad]
+    criterion = nn.CrossEntropyLoss(ignore_index = padding_index)
     optimizer = optim.RMSprop(model.parameters(), lr=config.learning_rate)
 
+    perplexity = 0
     losses = []
     accuracies = []
     for epoch in range(config.n_epochs):
-        for step, (batch, _) in enumerate(data_loader):
+        for step, (batch, sentence_len) in enumerate(train_data_loader):
             # Only for time measurement of step through network
             t1 = time.time()
             batch_inputs, batch_targets = batch
             batch_inputs = torch.stack(batch_inputs).to(device)
-            batch_targets = torch.stack(batch_targets).to(device).view(-1)
+            flat_targets = torch.stack(batch_targets).to(device).view(-1)
+
             output, _ = model(batch_inputs)
-
             output = output.view(batch_inputs.size()[0]*batch_inputs.size()[1], -1)
-            accuracy = calculate_accuracy(output ,batch_targets)
-
-            loss = criterion(output, batch_targets)
+            accuracy = calculate_accuracy(output, flat_targets)
+            
+            loss = criterion(output, flat_targets)
+            perplexity += np.exp(loss.item())/ torch.sum(sentence_len).item()
+            
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+
+            #total_perplexity, total_negative_ll, accuracy = dataset_metrics(eval_data_loader, model)
+            #print("total_perplexity:{}, total_negative_ll:{}, accuracy:{}".format(total_perplexity, total_negative_ll, accuracy))
 
             # For plotting
             # accuracies.append(accuracy)
@@ -123,20 +171,24 @@ def train(config):
             examples_per_second = config.batch_size/float(t2-t1)
             if step % config.print_every == 0:
 
-                print("[{}] Epoch {} Train Step {:00f}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                    "Accuracy = {:.2f}, Loss = {:.3f}".format(
+                print("[{}] Epoch {} Train Step {:00f}, Batch Size = {}, Examples/Sec = {:.2f},"
+                    "Loss = {:.3f}".format(
                         datetime.now().strftime("%Y-%m-%d %H:%M"), epoch, step,
                         config.batch_size, examples_per_second,
-                        accuracy, loss
+                        loss
                 ))
 
             if step % config.sample_every == 0:                
                 generated_sample = generate_samples(model,
-                                                    dataset=dataset,
+                                                    dataset=train_dataset,
                                                     device=device,
                                                     temperature=config.temperature,
                                                     method=config.sample_method)
-                print("SAMPLE:", dataset.convert_to_string(generated_sample))
+                print("SAMPLE:", train_dataset.convert_to_string(generated_sample))
+        print(perplexity)
+        # total_perplexity, total_negative_ll, accuracy = dataset_metrics(eval_data_loader, model)
+        # print("total_perplexity:{}, total_negative_ll:{}, accuracy:{}".format(
+        #     total_perplexity, total_negative_ll, accuracy))
 
             #     # f.write(str(step) + ": " + out + "\n")
 
